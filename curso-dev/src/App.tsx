79ca55f1f7826d1468eaa2e6761d66abeeb62a35
LucasMeloTorres <lucasmelotorres@gmail.com>
2025-08-19 21:08:00 -0300

    feat: add Main component with learning objectives and images

--- curso-dev/src/App.tsx
+++ curso-dev/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import MenuNav from "./components/MenuNav";
 import Banner from "./components/Banner";
+import Main from "./components/main";
 
 function App() {
   return (
     <>
       <MenuNav />
       <Banner />
+      <Main />
     </>
   );
 }
---
+++ curso-dev/src/components/Main.tsx
@@ -0,0 +1,88 @@
+function Main() {
+  return (
+    <div className="main-container">
+      <h2>O que você vai aprender</h2>
+
+      <div className="img-content">
+        <div className="img-content-individual">
+          <img
+            className="img-main"
+            src="./../src/assets/img/smartphone.png"
+            alt="Imagem de Smartphone"
+          />
+          <p>
+            Fazer Manutenção em todos os Aparelhos Apple da forma correta e
+            profissional;
+          </p>
+        </div>
+        <div className="img-content-individual">
+          <img
+            className="img-main"
+            src="./../src/assets/img/smartphone.png"
+            alt="Imagem de Smartphone"
+          />
+          <p>
+            Montar o seu próprio negócio sólido e eficiente começando do zero
+            com toda parte administrativa;
+          </p>
+        </div>
+        <div className="img-content-individual">
+          <img
+            className="img-main"
+            src="./../src/assets/img/smartphone.png"
+            alt="Imagem de Smartphone"
+          />
+          <p>
+            Consertos de Placas como Ci de áudio, Ci de carga, Baseband e
+            outros;
+          </p>
+        </div>
+        <div className="img-content-individual">
+          <img
+            className="img-main"
+            src="./../src/assets/img/smartphone.png"
+            alt="Imagem de Smartphone"
+          />
+          <p>Desbloqueio de iPhones em Geral;</p>
+        </div>
+        <div className="img-content-individual">
+          <img
+            className="img-main"
+            src="./../src/assets/img/smartphone.png"
+            alt="Imagem de Smartphone"
+          />
+          <p>
+            Troca de componentes como tela e bateria sem a mensagem “Não é
+            possível verificar se o componente é genuíno”;
+          </p>
+        </div>
+        <div className="img-content-individual">
+          <img
+            className="img-main"
+            src="./../src/assets/img/smartphone.png"
+            alt="Imagem de Smartphone"
+          />
+          <p>Trocar Vidro Traseiro de iPhones;</p>
+        </div>
+        <div className="img-content-individual">
+          <img
+            className="img-main"
+            src="./../src/assets/img/smartphone.png"
+            alt="Imagem de Smartphone"
+          />
+          <p>Trocar Vidro Traseiro de iPhones;</p>
+        </div>
+        <div className="img-content-individual">
+          <img
+            className="img-main"
+            src="./../src/assets/img/smartphone.png"
+            alt="Imagem de Smartphone"
+          />
+          <p>Trocar Vidro Traseiro de iPhones;</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
